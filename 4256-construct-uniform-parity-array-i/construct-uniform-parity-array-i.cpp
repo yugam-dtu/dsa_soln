87d26d903399a5074e237f00a2cb7ff4e6ab8b2a
Yugam Grover <YUGROV@GMAIL.COM>
2026-09-02 13:31:11 +0530

    Time: 0 ms (100.00%) | Memory: 30.2 MB (94.95%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        // int oddcount=0;
+        // int evencount=0;
+        // for(int i=0;i<nums1.size();i++){
+        //     if(nums1[i]%2==0){
+        //         evencount++;
+        //     }
+        //     else{oddcount++;}
+            
+        // }
+        // if(evencount==1||oddcount==1){
+        //     return fasle;
+        // }
+        // else true;
+        return true;
+        
+        
+    }
+};
