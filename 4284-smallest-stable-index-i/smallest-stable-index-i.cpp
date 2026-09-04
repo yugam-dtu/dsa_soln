c89df34614da88be6b1c358a362afe52dbb8266e
Yugam Grover <YUGROV@GMAIL.COM>
2026-09-04 13:08:50 +0530

    Time: 0 ms (100.00%) | Memory: 31.1 MB (56.73%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -1,44 +1,16 @@
 class Solution {
-    int maxm(vector<int>& nums, int j) {
-        int maxm2 = 0;
-        for (int i = 0; i <= j; i++) {
-            if (maxm2 < nums[i]) {
-                maxm2 = nums[i];
-            }
-        }
-        return maxm2;
-    }
-    int minm(vector<int>& nums, int j) {
-        int minm2 = INT_MAX;
-        for (int i = j; i < nums.size(); i++) {
-            if (minm2 > nums[i]) {
-                minm2 = nums[i];
-            }
-        }
-        return minm2;
-    }
-
 public:
     int firstStableIndex(vector<int>& nums, int k) {
-        vector<int> arr;
-        int j = 0;
-        while (j < nums.size()) {
-            int maxi = maxm(nums, j);
-            int mini = minm(nums, j);
-            int z = maxi - mini;
+        int n = nums.size();
+        vector<int> suffMin(n);
+        suffMin[n-1] = nums[n-1];
+        for (int i = n-2; i >= 0; i--)
+            suffMin[i] = min(nums[i], suffMin[i+1]);
 
-            arr.push_back(z);
-
-            j++;
-        }
-        if (arr.size() == 0) {
-            return -1;
-        }
-        for (int i = 0; i < arr.size(); i++) {
-            if (arr[i] <= k) {
-                return i;
-            }
-            
+        int prefMax = nums[0];
+        for (int i = 0; i < n; i++) {
+            prefMax = max(prefMax, nums[i]);
+            if (prefMax - suffMin[i] <= k) return i;
         }
         return -1;
     }
